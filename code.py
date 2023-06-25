5e84029c17e0a458857c256e5140d07735af81b0
Sean O. Stalley <sstalley@pdx.edu>
2023-06-25 10:44:11 -0700

    Added voltage monitoring

--- code.py
+++ code.py
@@ -9,6 +9,7 @@ from rainbowio import colorwheel
 import neopixel
 import pwmio
 import digitalio
+from analogio import AnalogIn
 
 print(dir(board))
 
@@ -25,16 +26,30 @@ num_pixels = 1
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
 
+analog_in = AnalogIn(board.A2)
 
+def get_voltage(pin):
+    rv1 = rv2 = 15000 # set these to the resistor values you chose
+    vdiv = rv1 / (rv1 + rv2)
+    return (pin.value * 3.3) / 65536 / vdiv
+
+b_voltage = get_voltage(analog_in)
 
 def rainbow_cycle(wait):
     global old_value
     global pwm
+    global b_voltage
+    old_b_voltage = b_voltage
     for j in range(255):
         for i in range(num_pixels):
             rc_index = (i * 256 // num_pixels) + j
             pixels[i] = colorwheel(rc_index & 255)
         pixels.show()
+
+        b_voltage = get_voltage(analog_in)
+        if not (b_voltage == old_b_voltage):
+            print("Battery Voltage:", "{:.2f}".format(b_voltage))
+            old_b_voltage = b_voltage
 
         if not (switch.value == old_value):
             if not switch.value:
